30574a33d5fe90e5b673db2a675536fe1d739b32
Efremova Aleksandra <doctor.akab@gmail.com>
2024-08-16 17:17:29 +0300

    Lesson3 : Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Создаём функцию, которая будет обнулять чётные элементы массива
+void ZeroEventElement(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            arr[i] = 0;
+        }
+    }
+}
+// Создаём функцию для вывода нового массива
+void PrintArray(int[] arr)
+{
+    foreach (int e in arr)
+    {
+        System.Console.Write($"{arr[e]} ");
+    }
+}
+
+int[] array = { 1, 2, 3, 4, 5, 6, 7, 8 };
+ZeroEventElement(array);
+PrintArray(array);
